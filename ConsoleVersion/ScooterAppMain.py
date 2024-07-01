feb6d9b42e36b320f2b4a9c8050ac50d7a3c3792
LukasDano <lukas.danowski@mail.de>
2024-07-01 16:15:12 +0200

    exit hinzugefügt

--- ConsoleVersion/ScooterAppMain.py
+++ ConsoleVersion/ScooterAppMain.py
@@ -10,14 +10,20 @@ while True:
     if __name__ == "__main__":
 
         print("Optionen: \n")
+        print("0. Beenden")
         print("1. Scooter ausleihen")
         print("2. Daten der aktuellen Fahrt abfragen")
 
+
         print("\n")
         auszuführendeMehtode = input("Was möchtest du machen?")
+
+        if auszuführendeMehtode == "0" or auszuführendeMehtode == "exit" or auszuführendeMehtode == "beenden":
+            break
 
         if auszuführendeMehtode == "1" or auszuführendeMehtode == "ausleihen":
             app.scooterAusleihen()
         
         if auszuführendeMehtode == "2" or auszuführendeMehtode == "abfragen":
             app.datenZurAktuellenFahrt()
+
