c70d71299190e190578669578f205f4700abe77b
LukasDano <lukas.danowski@mail.de>
2024-07-02 10:09:00 +0200

    Rückgabe hinzugefügt

--- ConsoleVersion/ScooterRentalApp.py
+++ ConsoleVersion/ScooterRentalApp.py
@@ -4,6 +4,7 @@ gebuehr = 1
 pricePerMinute = 0.10
 
 ausleihZeitpunkt = [0, 0, 0]
+rueckgabeZeitpunkt = [0, 0, 0]
 scooterAusgeliehen = False
 
 def getCurrentTimeStamp():
@@ -14,16 +15,14 @@ def getCurrentTimeStamp():
     aktuellerZeitpunkt = [int(hours), int(minutes), int(seconds)]
     return aktuellerZeitpunkt
 
-def getTimeDifferance():
+def getTimeDifferance(fruehererZeitpunkt, spaetererZeitpunkt):
 
-    zeitpunkt1 = ausleihZeitpunkt
-    zeitpunkt2 = getCurrentTimeStamp()
     differenz = [0, 0]
-    
-    differenz[0] = zeitpunkt2[0] - zeitpunkt1[0]
-    differenz[1] = zeitpunkt2[1] - zeitpunkt1[1]
 
-    print("Differenz:", differenz)
+    differenz[0] = spaetererZeitpunkt[0] - fruehererZeitpunkt[0]
+    differenz[1] = spaetererZeitpunkt[1] - fruehererZeitpunkt[1]
+
+    #print("Differenz:", differenz)
     return differenz
 
 def getPrice(timeInMinutes):
@@ -62,7 +61,10 @@ def datenZurAktuellenFahrt():
     print("- Die Daten zu deiner aktuellen Fahrt -")
     print("\n")
 
-    timeDifference = getTimeDifferance()
+    fruehererZeitpunkt = ausleihZeitpunkt
+    spaetererZeitpunkt = getCurrentTimeStamp()
+
+    timeDifference = getTimeDifferance(fruehererZeitpunkt, spaetererZeitpunkt)
     hours = timeDifference[0]
     minutes = timeDifference[1]
     seconds = getCurrentTimeStamp()[2]
@@ -76,3 +78,32 @@ def datenZurAktuellenFahrt():
 
     price = getPrice(timeInMinutes)
     print(f"Aktueller Preis dieser Fahrt: {price}€")
+
+def scooterZurueckgeben():
+    rueckgabeZeitpunkt = getCurrentTimeStamp()
+
+    global scooterAusgeliehen
+    scooterAusgeliehen = False
+
+    print("\n\n")
+    print("- Scooter Zurueckgeben -")
+    print("\n")
+
+    print(f"Scooter zurueckgeben am: {rueckgabeZeitpunkt[0]}:{rueckgabeZeitpunkt[1]} Uhr")
+
+    timeDifference = getTimeDifferance(ausleihZeitpunkt, rueckgabeZeitpunkt)
+    hours = timeDifference[0]
+    minutes = timeDifference[1]
+    seconds = getCurrentTimeStamp()[2]
+
+    print(f"Insgesamt ausgeliehene Zeit: {hours} Stunden {minutes} Minuten")
+    #print("Seconds", seconds)
+
+    timeInMinutes = hours * 60 + minutes
+    
+    if seconds != 0:
+        timeInMinutes += 1
+
+    price = getPrice(timeInMinutes)
+    print(f"Preis dieser Fahrt: {price}€")
+    
