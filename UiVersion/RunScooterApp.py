507efb9660325b5bd656647d8d7c422a96397477
LukasDano <lukas.danowski@mail.de>
2024-07-09 16:59:15 +0200

    first window ui

--- UiVersion/RunScooterApp.py
+++ UiVersion/RunScooterApp.py
@@ -1,11 +1,6 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 from datetime import *
-import pystray
-from pystray import MenuItem as item
-import threading
-import sys
-
 
 import ScooterRentalAppUi
 
@@ -14,30 +9,41 @@ ctk.set_default_color_theme("dark-blue")
 
 app = ScooterRentalAppUi
 
-def close():
-    root.destroy()
+class frontPage:
+    root = ctk.CTk()
+    root.title("ScooTeq App")
+    root.iconbitmap("scooTecIcon.ico")
+    root.geometry("500x350")
 
-def runTheApp():
-    root.geometry("650x400")
-    root.mainloop()
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-def update_time():
-    current_time = app.getCurrentTimeStamp()
-    textField.configure(text=current_time)
-    root.after(1000, update_time)
+    label = ctk.CTkLabel(master=frame, text="Was möchtest du machen?", font=("Helvetica", 15, "bold"))
+    label.pack(pady=12, padx=10)
 
-root = ctk.CTk()
-root.title("ScooTeq App")
-root.iconbitmap("scooTecIcon.ico")
+    ausleihen_button = ctk.CTkButton(master=frame, text="ausleihen", command=app.scooterAusleihen)
+    ausleihen_button.pack(pady=12, padx=18)
+    
+    reservieren_button = ctk.CTkButton(master=frame, text="reservieren", command=app.scooterReservieren)
+    reservieren_button.pack(pady=12, padx=18)
+    
+    reservieren_button = ctk.CTkButton(master=frame, text="Beenden", command=root.destroy)
+    reservieren_button.pack(pady=12, padx=18)
 
-textField = ctk.CTkLabel(root, text="", font=("Arial", 48))
-textField.pack(pady=20)
+frontPage.root.mainloop()
 
+class secondPage:
+    root = ctk.CTk()
+    root.title("ScooTeq App")
+    root.iconbitmap("scooTecIcon.ico")
+    root.geometry("500x350")
 
-# Frame for scooter selection
-selection_frame = ctk.CTkFrame(root)
+    def update_time():
+        exampleTimeField.configure(text=app.getCurrentTimeStamp())
+        root.after(1000, update_time)
 
-update_time()
+    exampleTimeField = ctk.CTkLabel(master=root, text="", font=("Calibri", 23))
+    update_time()
+    exampleTimeField.pack(pady=20)
 
-if __name__ == "__main__":
-    runTheApp()
+secondPage.root.mainloop()
